7cda88dd1ae2e855651f735e2fa37a518b89a6f5
KokiYamanaka <liewguangyi2001@gmail.com>
2026-06-27 14:37:25 +0900

    Allow all *.vercel.app origins in CORS to fix preview deploy URLs

--- server/server.js
+++ server/server.js
@@ -48,7 +48,17 @@ console.log("Allowed origins:", allowedOrigins);
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        /\.vercel\.app$/.test(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
